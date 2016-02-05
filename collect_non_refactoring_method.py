4aa986d48fbb6938a99166af712078adf4a9b14f
Daiki Tanaka <hangetuita@gmail.com>
2016-02-05 15:54:31 +0900

    modified collect_non_refactoring_method.py

--- collect_non_refactoring_method.py
+++ collect_non_refactoring_method.py
@@ -11,30 +11,22 @@ def collect_non_refactoring_method(historage, num_of_methods):
         commits.extend(list(historage.iter_commits(ref)))
 
     while len(non_refactoring_methods) < num_of_methods:
-        random_commit = commits[random.randint(0, len(commits) - 1)]
+        random_commit = random.choice(commits)
         if not random_commit.parents:
             continue
-        p = random_commit.parents[0]
-        extract_method_information = detect_extract_method_from_commit(p, random_commit, historage)
-        for f in p.tree.trees[random.randint(0, len(p.tree.trees) - 1)].traverse():
+        p = random.choice(random_commit.parents)
+        method_candidates = []
+        for f in p.tree.traverse():
             if f.type == 'blob':
                 if is_method_body(f.path) or is_constructor_body(f.path):
-                    if is_method_body(f.path):
-                        method = get_method(f.path)
-                    else:
-                        method = get_constructor(f.path)
-                    method_information = get_method_information(method)
+                    assert f.path.split('/')[0].endswith('.java')
+                    method_candidates.append(f.path)
 
-                    if extract_method_information:
-                        for e in extract_method_information:
-                            if e['extracted_method_path'] == f.path:
-                                del method
-                        if method:
-                            non_refactoring_methods.append(method_information)
-                            break
-                    else:
-                        non_refactoring_methods.append(method_information)
-                        break
+        non_refactoring_methods.append(random.choice(method_candidates))
+        extract_method_information = detect_extract_method_from_commit(p, random_commit, historage)
+        if any((e['extracted_method_path'] == non_refactoring_methods[-1] for e in extract_method_information)):
+            non_refactoring_methods.pop(-1)
+            break
 
     return non_refactoring_methods
 
